4b2998940a1142953b1eb406b4a9d8756f97027d
minig0lem <gmltjd0326@naver.com>
2023-05-17 16:59:49 +0900

    first commit

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const model = require('../models');
-const path = require('path');
+
 
 exports.getLoginPage = async(req, res, next) => {
     
-    if(req.session.student_id) {
+    if(req.session.student_id) {        //로그인 상태
         res.send({login: true});
     }
     else {
-        res.send({login: false});
+        res.send({login: false});       //로그아웃 상태
     }
 };
 
 exports.Login = async(req, res, next) => {
-    let id = req.body.student_number;
+    let id = req.body.userNumber;
     let pw = req.body.password;
-    let user = await model.student.findAll({where: {student_id: id, pw:pw}}).catch((err) => console.log(err));   
+    let user = await model.students.findAll({where: {student_id: id, pw:pw}}).catch((err) => console.log(err));   
 
-  if (user.length !== 0) {
-    console.log(result);
+  if (user.length !== 0) {        //로그인 성공
     req.session.student_id = id;
-    res.json({ user: user, result: true });
-  } else {
-    res.json({ result: false });
+    console.log(req.session);
+    res.send({ user: user, result: true });
+  } else {                        //로그인 실패
+    res.send({ result: false });
   }
 };
