98a2adcf819fbffb18d856e4fa4153df837f49cd
minig0lem <gmltjd0326@naver.com>
2023-05-24 20:15:34 +0900

    내 정보 수정 기능 구현

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const model = require('../models');
 
+//비밀번호 체크 함수
+exports.CheckPW = async(req, res, next) => {
+    let id = req.body.userNumber;
+    let pw = req.body.password;
+    //비밀번호 체크
+    pw_check = await model.students.findAll({ where: { student_id: id, pw:pw } }).catch((err) => console.log(err));
+    if (pw_check.length !== 0) {        
+        //비밀번호 일치
+        res.sendStatus(200);
+    } else {
+        //비밀번호 불일치
+        res.sendStatus(401);  //unauthorized              
+    }
+};
+
+exports.updateUser = async(req, res, next) => {
+    
+};
