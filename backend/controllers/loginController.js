f587204ab9cedb1b41ab210e2e18b7a82aebbf39
minig0lem <gmltjd0326@naver.com>
2023-06-16 23:33:23 +0900

    제출 전 수정

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -4,10 +4,13 @@ const model = require("../models");
 //로그인 체크 함수
 exports.CheckLogin = async (req, res, next) => {
   if (req.session.loginId) {
-    //로그인 상태
-    return res.sendStatus(200);
+      if(req.session.userType === 'student') {
+          return res.sendStatus(200);
+      } else if (req.session.userType === 'professor'){
+          return res.sendStatus(201);
+      }
   } else {
-    return res.sendStatus(401); //로그아웃 상태
+          return res.sendStatus(401); //로그아웃 상태
   }
 };
 //로그인 함수
